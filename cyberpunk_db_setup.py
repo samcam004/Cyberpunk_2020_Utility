7a3e348036e16c32b099c9dc3ea2fc3ba7b0bb6c
samcam004 <samcam@umich.edu>
2024-07-13 10:38:28 -0400

    Weapons List Complete
    Renamed db_setup .py to cyberpunk_db_setup.py
    Created weapons_info.py

--- cyberpunk_db_setup.py
+++ cyberpunk_db_setup.py
@@ -1,4 +1,5 @@
 import sqlite3 as sq
+from weapons_info import weapons_array
 
 
 def create_database():
@@ -80,14 +81,76 @@ def create_database():
     conn.close()
 
 
-def fill_tables():
+def fill_bodyarmor_table():
+
     conn = sq.connect("cyberpunk2020.db")
     cursor = conn.cursor()
+
+    armor_array = [
+        ["Cloth, leather√", "Arms, Torso, possibly legs", 0, 0, "Varies"],
+        ["Heavy leather", "Arms, Torso, possibly legs", 4, 0, "50.00"],
+        ["Kevlar T-Shirt, Vest√", "Torso", 10, 0, "90.00"],
+        ["Steel helmet", "Head", 14, 0, "20.00"],
+        ["Light Armor jacket√", "Torso, Arms", 14, 0, "150.00"],
+        ["Med Armor jacked√", "Torso, Arms", 18, 1, "200.00"],
+        ["Flack vest", "Torso", 20, 1, "200.00"],
+        ["Flack pants", "Legs", 20, 1, "200.00"],
+        ["Nylon helmet", "Head", 20, 0, "100.00"],
+        ["Heavy Armor jacket√", "Torso, Arms", 20, 2, "250.00"],
+        ["Door Gunner's vest", "Torso", 25, 3, "250.00"],
+        ["MetalGear™", "Whole Body", 25, 2, "600.000"],
+    ]
+
+    # Insert the 2D array into the BodyArmor table
+    for row in armor_array:
+        try:
+            cursor.execute(
+                "INSERT OR IGNORE INTO BodyArmor (TypeOfArmor, Covers, SP, EV, Cost) VALUES (?, ?, ?, ?, ?)",
+                row,
+            )
+        except sq.IntegrityError as e:
+            print(f"IntegrityError: {e} - for row: {row}")
+        except sq.Error as e:
+            print(f"SQLite error: {e} - for row: {row}")
+
+    # Commit the changes
+    conn.commit()
+
+
+def fill_weapons_table():
+    conn = sq.connect("cyberpunk2020.db")
+    cursor = conn.cursor()
+
+    # Insert the 2D array into the BodyArmor table
+    for row in weapons_array:
+        try:
+            cursor.execute(
+                "INSERT OR IGNORE INTO Weapons (Category, Company, Name, Type, WeaponAccuracy, Concealability, Availability, Damage, Ammo, NumberOfShots, ROF, Reliability, Range, Cost, Description) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                row,
+            )
+        except sq.IntegrityError as e:
+            print(f"IntegrityError: {e} - for row: {row}")
+        except sq.Error as e:
+            print(f"SQLite error: {e} - for row: {row}")
+
+    # Commit the changes
+    conn.commit()
+
+
+def fill_gear_table():
+    """
+    Category TEXT NOT NULL,
+    Name TEXT PRIMARY KEY,
+    Cost TEXT NOT NULL,
+    Description TEXT NULL
+    """
 
 
 def main():
     print("Test")
     create_database()
+    fill_bodyarmor_table()
+    fill_weapons_table()
 
 
 if __name__ == "__main__":
